608d58a73b2197322e64eeecf8ea87805678b7d2
Dinal Fernando <dinal.ridma@gmail.com>
2024-04-16 07:18:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const bodyParser = require('body-parser')
+const path = require("path");
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+
+const adminData = require("./routes/admin")
+const shopRoutes = require("./routes/shop")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")))
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+
+app.listen(3000);
