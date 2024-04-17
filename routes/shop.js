1d71dc021cbe4da629f1d7d4b7972ef6b908d4bd
DinalFernando <dinal.ridma@gmail.com>
2024-04-17 10:08:19 +0530

    added

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,22 @@
-const path = require("path")
-const express = require("express")
-const rootDir = require("../util/path")
-const adminData = require("./admin");
-const {products} = require("./admin");
+const path = require('path');
 
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    // res.sendFile(path.join(rootDir, "views", "shop.html"))
-    res.render('shop', {prods: products, docTitle: 'My Shop'});
-})
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+});
 
 module.exports = router;
